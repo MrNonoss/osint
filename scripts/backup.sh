392de6306d5473a4231f5ef37007aefebb0930e6
MrNonoss <bruno.bordas@example.com>
2021-08-17 15:56:56 +0200

    removre scripts

--- scripts/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SRCDIR="/home/<username>/Documents/my_work/"
-DESTDIR="/home/<username>/Backups/"
-FILENAME=ug-$(date +%-Y%-m%-d)-$(date +%-T).tgz
-tar --create --gzip --file=$DESTDIR$FILENAME $SRCDIR
--- scripts/pipe.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-while true; do eval "$(cat /home/$USER/osint/html/pipe/pipe)"; done
